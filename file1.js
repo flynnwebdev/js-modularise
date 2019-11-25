8aa88a21edb90936eaf72b42fabd38dc0560aba9
Flynn <flynnwebdev@outlook.com>
2019-11-25 10:25:43 +1000

    Initial commit

---
+++ file1.js
@@ -0,0 +1,11 @@
+const unchangeableNumber = 1234;
+
+function add(x, y) {
+    return x + y
+}
+
+function subtract(x, y) {
+    return x - y
+}
+
+export default { unchangeableNumber, add, subtract }
---
+++ main.js
@@ -0,0 +1,7 @@
+import NewMath from "./file1.js"
+import _ from "lodash"
+
+// console.log(NewMath.add(5, 4))
+// console.log(NewMath.subtract(5, 4))
+// console.log(NewMath.unchangeableNumber)
+console.log(_.random(100, 300))
